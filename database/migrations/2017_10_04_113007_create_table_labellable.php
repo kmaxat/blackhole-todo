e016c013c0036de370cdb3bbbc1c4336d875cfa5
Maxat Ku <kmaxat@gmail.com>
2017-10-05 22:43:10 +0600

    Add Labellable table to manage many-to-many polymorphic relationship

---
+++ database/migrations/2017_10_04_113007_create_table_labellable.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableLabellable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('labellables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('label_id')->unsigned();
+            $table->foreign('label_id')->references('id')->on('labels');
+            $table->integer('labellable_id');
+            $table->string('labellable_type');
+            $table->unique(['label_id', 'labellable_id', 'labellable_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('labellables');
+    }
+}
